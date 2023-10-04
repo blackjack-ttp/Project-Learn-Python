5196ed807d9b4524cdede639ecdb7e9259fab70b
blackjack-ttp <phongtt120298@outlook.com>
2023-10-04 16:11:44 +0700

    #1 [chapte_1] add execrise 1: Enter 2 elements and calculate the total

---
+++ src/Chapter_1/Exercise_1.py
@@ -0,0 +1,5 @@
+# Xây dựng chương trình nhập 2 số a và b từ bàn phím, sau đó tình tổng và in kết quả ra màn hình
+a=float(input("Nhập số a: "))
+b=float(input("Nhập số b: "))
+S=a+b
+print("Tổng 2 số a và b là" , S)
