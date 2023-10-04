b93d01417ed4b8bdeb770932c31d6a4fca36293d
blackjack-ttp <phongtt120298@outlook.com>
2023-10-05 01:03:59 +0700

    #1 [chapter_1] add execrise 3: calculate circumference and area of circle

---
+++ src/Chapter_1/Execrise_3.py
@@ -0,0 +1,5 @@
+import math
+r=float(input("Nhập bán kính r = "))
+print('Chu vi hình tròn là: ', 2*math.pi*r)
+print('================================================================')
+print('Diễn tích hình tròn là: ', math.pi*r*r)
