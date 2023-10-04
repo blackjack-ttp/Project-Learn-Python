d838745e694627a9243702b4cbd30e795687401d
blackjack-ttp <phongtt120298@outlook.com>
2023-10-04 16:16:49 +0700

    #1 [chapte_1] add execrise : Enter 2 elements and calculate the a^n

---
+++ src/Chapter_1/Execrise_2.py
@@ -0,0 +1,5 @@
+# Xây dựng chương trình nhập vào hai số a, n từ bàn phím, sau đó tính an và in kết quả ra màn hình
+a=float(input("Nhập số a: "))
+n=float(input("Nhập số n: "))
+N=a**n 
+print("Kết quả", N)
