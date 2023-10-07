fb066b5407ab0c9b5d3893d8408760bae73f7fd5
blackjack-ttp <phongtt120298@outlook.com>
2023-10-07 23:12:23 +0700

    #4 [chapter_3] learn hàm và cách sử dụng

---
+++ src/Chapter_3/lesson_1.py
@@ -0,0 +1,10 @@
+# Hàm tính tổng a và b
+def tinh_tong(a, b):
+    tong = a + b
+    return tong
+
+# Sử dụng hàm
+x=5
+y=10
+result = tinh_tong(x, y)
+print("Tổng của ", x, " và ", y, " là ", result)
