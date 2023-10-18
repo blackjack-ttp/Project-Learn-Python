50639677ce041e82c78a26e7d035da7b7f0dee85
blackjack-ttp <phongtt120298@outlook.com>
2023-10-18 23:39:30 +0700

    #5 add chapter 4: lesson 1

---
+++ src/Chapter_4/Lesson_1.py
@@ -0,0 +1,9 @@
+# MẢNG LÀ GÌ?
+empty_list = [] #Khởi tạo danh sách rỗng
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] #Khởi tạo danh sách số nguyên
+names = ["Alice", "Bob", "Phong", "Tài"] #Khởi tạo danh sách chuỗi
+mixed_list = [1, "Alice", True] #Khởi tạo danh sách có các kiểu dữ liệu khác nhau
+print(empty_list)
+print(numbers)
+print(names)
+print(mixed_list)
